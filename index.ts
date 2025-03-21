72416c49fcf5aa70f07d0ce01e8aaf379cd39d24
LuongTT <luongtt@dipro.vn>
2025-03-21 11:45:34 +0700

    feat: add user services

---
+++ index.ts
@@ -0,0 +1,36 @@
+class UserRepository {
+  create(name: string, email: string) {
+    console.log("Saving to DB...");
+    const newUser = { name, email };
+    console.log(newUser);
+  }
+
+  findOneById(id: number) {
+    console.log("Fetching from DB...");
+  }
+}
+
+class EmailService {
+  sendWelcomeEmail(email: string) {
+    console.log("Sending email...");
+  }
+}
+
+class UserService {
+  constructor(
+    private userRepository: UserRepository,
+    private emailService: EmailService
+  ) {}
+
+  createUser(name: string, email: string) {
+    this.userRepository.create(name, email);
+    this.emailService.sendWelcomeEmail(email);
+  }
+
+  getUserById(id: number) {
+    this.userRepository.findOneById(id);
+  }
+}
+
+const userService = new UserService(new UserRepository(), new EmailService());
+userService.createUser("John", "johnwick@gmail.com");
